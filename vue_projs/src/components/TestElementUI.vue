6b0dcf3f84ad06b01c9daea262e913b63e6d7928
Songxiao Yang <48151761+BITCS0479@users.noreply.github.com>
2023-09-09 23:07:30 +0900

    Initial Commit

---
+++ vue_projs/src/components/TestElementUI.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    You have deployed a Vue project with element UI.
+  </div>
+  <div>
+    <el-button @click="show()">Test</el-button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+export default {
+  name: 'TestElementUI',
+  components: {},
+  methods:{
+    show() {
+      console.log("UI successful");
+    },
+  },
+}
+</script>
